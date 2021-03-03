49767055210273fda2399a78484b3c78e5f42c8a
Gustavo Borges <gustavo.borges.devfc@gmail.com>
2021-03-03 16:51:07 -0300

    Orion Adjustments

--- producao/0-ajp-web-common.js
+++ producao/0-ajp-web-common.js
@@ -1,30 +1,7 @@
-(function() {
+/**
+ * Avanti Comunicação <contato@penseavanti.com.br>
+ * almeidajunior
+ * @date Wed Mar 03 2021 16:50:27 GMT-0300 (GMT-03:00)
+ */
 
-    try {
-
-        let Common = {
-            init: function () {
-                Common.applyConfigInSearchField();
-            },
-            applyConfigInSearchField: () => {
-                let input = $(".search__input");
-                input.on('click', function () {
-                    $(this).val(' ');
-                });
-                $(document).click(function(event) {
-                    if (!$(event.target).is(".search__input")) {
-                        input.val('');
-                        // let thisInput = $('.search__input')[0];
-                        // thisInput.placeholder = "O QUE VOCÊ DESEJA ENCONTRAR?"; 
-                    }
-                });
-            }
-        }
-
-        // Instanciando a funcao
-        $(document).ready(Common.init)
-    } catch (e) {
-        console.log('Erro na instancia [Common]: ', e);
-    }
-
-})();
+"use strict";!function(){try{var o={init:function(){o.applyConfigInSearchField(),o.returnPreviousPage()},applyConfigInSearchField:function(){var e=$(".search__input");e.on("click",function(){$(this).val(" ")}),$(document).click(function(o){$(o.target).is(".search__input")||e.val("")})},returnPreviousPage:function(){localStorage.AJP_prevUrl&&localStorage.AJP_prevUrl.length?(console.log("[IF]"),$("#return-to-prev-page").attr("href",localStorage.getItem("AJP_prevUrl")),localStorage.setItem("AJP_currentUrl",window.location.href),$(window).unload(function(){localStorage.setItem("AJP_prevUrl",window.location.href)})):(console.log("[ELSE]"),window.localStorage.setItem("AJP_prevUrl",window.location.href),window.localStorage.setItem("AJP_currentUrl",window.location.href))}};$(document).ready(o.init)}catch(o){console.log("Erro na instancia [Common]: ",o)}}();
