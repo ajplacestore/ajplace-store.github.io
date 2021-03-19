2679cdd7ee74430891612b9a0b562dc200a1912d
Gustavo Borges <gustavo.borges.devfc@gmail.com>
2021-03-19 13:34:11 -0300

    Orion Adjustments

---
+++ producao/ajp-cart.js
@@ -0,0 +1,7 @@
+/**
+ * Avanti Comunicação <contato@penseavanti.com.br>
+ * almeidajunior
+ * @date Fri Mar 19 2021 13:33:45 GMT-0300 (GMT-03:00)
+ */
+
+"use strict";!function(){try{var n={init:function(){n.hello()},hello:function(){console.log("Cart Is On")}};$(document).ready(n.init)}catch(n){console.log("Erro na instancia [Cart]: ",n)}}();
