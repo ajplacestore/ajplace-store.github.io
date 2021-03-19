09fd18f8c051604d744ea14d5936fa000b6e4924
Gustavo Borges <gustavo.borges.devfc@gmail.com>
2021-03-19 15:43:44 -0300

    Orion Adjustments

--- producao/ajp-cart.js
+++ producao/ajp-cart.js
@@ -1,7 +1,7 @@
 /**
  * Avanti Comunicação <contato@penseavanti.com.br>
  * almeidajunior
- * @date Fri Mar 19 2021 13:33:45 GMT-0300 (GMT-03:00)
+ * @date Fri Mar 19 2021 15:43:04 GMT-0300 (GMT-03:00)
  */
 
-"use strict";!function(){try{var n={init:function(){n.hello()},hello:function(){console.log("Cart Is On")}};$(document).ready(n.init)}catch(n){console.log("Erro na instancia [Cart]: ",n)}}();
+"use strict";!function(){try{var n={init:function(){n.toggleCart()},toggleCart:function(){$(".header__minicart-icon").off();var n=$("#ajp-cart-close"),c=$("#ajp-cart-continuebuy"),o=$(".header__minicart-icon");n.on("click",function(){$(document.body).removeClass("cart-on")}),o.on("click",function(){$(document.body).addClass("cart-on")}),c.on("click",function(){$(document.body).removeClass("cart-on")})}};$(document).ready(n.init)}catch(n){console.log("Erro na instancia [Cart]: ",n)}}();
